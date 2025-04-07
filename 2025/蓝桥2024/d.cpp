4c3dfeeeb27d4c8d04b274b26196568962bdf891
夏尾草 <1910541664@qq.com>
2025-04-07 20:39:59 +0800

    晦气傻逼蓝桥

---
+++ 2025/蓝桥2024/d.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+const char nl = '\n';
+  typedef long long ll;
+  typedef long double ld;
+using i64 = unsigned long long;
+using i32 = unsigned;
+using i128 = unsigned __int128;
+  #define all(x) (x).begin(), (x).end()
+void solve(){
+//解决示例具体方法 
+}
+
+
+int main(){
+ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+int t = 1;
+cin >> t;
+while(t--){
+    solve();
+}
+    return 0;
+}
